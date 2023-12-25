597f91504c29abd01b22b1af6f2eb52450918556
morisoinc <64343733+morisoinc@users.noreply.github.com>
2023-12-25 17:55:04 -0300

    calculating and showing the amount per person

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -14,6 +14,17 @@ struct ContentView: View {
 
     let tipPercentages = [10, 15, 20, 25, 0]
 
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+
     var body: some View {
         NavigationStack {
             Form {
@@ -40,6 +51,10 @@ struct ContentView: View {
                         }
                     }
                     .pickerStyle(.navigationLink)
+                }
+                Section("Amount per person") {
+                    Text(totalPerPerson,
+                         format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
             .navigationTitle("WeSplit")
